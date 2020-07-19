fadc47d51cb1a557c721691bfd00d7aa2459cc49
amarin <17020181+asiripanich@users.noreply.github.com>
2020-07-20 00:31:48 +1000

    remove emptied scripts

--- R/mod_gauge_box.R
+++ R/mod_gauge_box.R
@@ -1,4 +1,4 @@
-#' gauge_box UI Function
+#' gauge UI Function
 #'
 #' @description A shiny Module.
 #'
@@ -7,24 +7,39 @@
 #' @noRd 
 #'
 #' @importFrom shiny NS tagList 
-mod_gauge_box_ui <- function(id){
+mod_gauge_ui <- function(id){
   ns <- NS(id)
   tagList(
- 
+    box(flexdashboard::gaugeOutput(ns("gauge")), title="gauge", width = 3)
   )
 }
     
-#' gauge_box Server Function
+#' gauge Server Function
 #'
 #' @noRd 
-mod_gauge_box_server <- function(input, output, session, value, min, max, color){
+mod_gauge_server <- function(input, output, session, value, max_value = 100, title = ""){
   ns <- session$ns
- 
+  # output$my_title <- title
+  output$gauge <- flexdashboard::renderGauge({
+    flexdashboard::gauge(
+      value,
+      min = 0,
+      max = max_value,
+      symbol = '%',
+      label = paste("Test Label"),
+      flexdashboard::gaugeSectors(
+        success = c(100, 6),
+        warning = c(5, 1),
+        danger = c(0, 1),
+        colors = c("#CC6699")
+      )
+    )
+  })
 }
     
 ## To be copied in the UI
-# mod_gauge_box_ui("gauge_box_ui_1")
+# mod_gauge_ui("gauge_ui_1")
     
 ## To be copied in the server
-# callModule(mod_gauge_box_server, "gauge_box_ui_1")
+# callModule(mod_gauge_server, "gauge_ui_1")
  
