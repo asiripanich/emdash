ee7bec64f7c7f6629e29cf6f450b6e1e9f2750b6
amarin <17020181+asiripanich@users.noreply.github.com>
2020-10-15 17:02:48 +1100

    remove a non imported package from a test script

--- tests/testthat/test-utils_tidy_data.R
+++ tests/testthat/test-utils_tidy_data.R
@@ -11,7 +11,10 @@ test_that("tidy_*", {
   tidied_participants = 
     tidy_participants(query_stage_profiles(cons), query_stage_uuids(cons))
   
-  checkmate::expect_data_table(summarise_trips(tidied_participants, tidied_cleaned_trips),
-                               min.rows = 1)
+  summary_df = summarise_trips(tidied_participants, tidied_cleaned_trips)
   
+  expect_true(is.data.table(summary_df))
+  expect_true(nrow(summary_df) >= 1)
+  expect_true(ncol(summary_df) >= 1)
+
 })
