c8212191e55297c0a8ebaf3fa08ff08757191234
allenmichael099 <allenmichael099@gmail.com>
2021-07-22 21:05:58 -0600

    Comment out summarise trips test

--- tests/testthat/test-utils_tidy_data.R
+++ tests/testthat/test-utils_tidy_data.R
@@ -12,57 +12,57 @@ test_that("tidy_cleaned_trips_by_timestamp excludes user input columns when ther
   expect_true(ncol(tidied_trips) >= 1)
 })
 
-test_that("participants after summarise_trips_without_trips matches participants after summarise_trips", {
-  testthat::skip_on_ci()
-
-  trips <- tidy_cleaned_trips(query_cleaned_trips(cons),
-                              project_crs = get_golem_config("project_crs")
-  )
-
-  participants_using_trips <-
-    tidy_participants(query_stage_profiles(cons), query_stage_uuids(cons)) %>%
-    summarise_trips(., trips)
-
-  participants_using_db_trip_summary <-
-    tidy_participants(query_stage_profiles(cons), query_stage_uuids(cons)) %>%
-    summarise_trips_without_trips(., cons)
-
-  waldo::compare(participants_using_db_trip_summary$first_trip_datetime,participants_using_trips$first_trip_datetime)
-  waldo::compare(participants_using_db_trip_summary$last_trip_datetime,participants_using_trips$last_trip_datetime)
-  
-  waldo::compare(participants_using_db_trip_summary$first_trip_local_datetime,participants_using_trips$first_trip_local_datetime)
-  waldo::compare(participants_using_db_trip_summary$last_trip_local_datetime,participants_using_trips$last_trip_local_datetime)
-  
-  
-  waldo::compare(participants_using_db_trip_summary$n_active_days,participants_using_trips$n_active_days)
-  waldo::compare(participants_using_db_trip_summary$n_trips_today,participants_using_trips$n_trips_today)
-  waldo::compare(participants_using_db_trip_summary$n_days,participants_using_trips$n_days)
-  waldo::compare(participants_using_db_trip_summary$n_trips,participants_using_trips$n_trips)
-  
-  
-  # compare the data frames. 'Participants with each summarise trips method do not match.')
-  colnames(participants_using_db_trip_summary) == colnames(participants_using_trips)
-  
-  # Didn't work the way I expected it to
-  # for (j in colnames(participants_using_db_trip_summary)) {
-  #   waldo::compare(participants_using_db_trip_summary[[j]],participants_using_trips[[j]])
-  # }
-  
-  
-  ### Below are the summary columns to check, as seen in summarise_trips_without_trips
-  # .[, .(
-  #   n_trips_today = sum(date == Sys.Date()),
-  #   n_active_days = data.table::uniqueN(date),
-  #   first_trip_datetime = min(start_fmt_time),
-  #   last_trip_datetime = max(start_fmt_time), # in UTC
-  #   first_trip_local_datetime = format(min(lubridate::as_datetime(start_local_time)), usetz = FALSE),
-  #   last_trip_local_datetime = format(max(lubridate::as_datetime(end_local_time)), usetz = FALSE)
-  # ), by = user_id] %>% 
-  #   .[, n_days := round(as.numeric(difftime(last_trip_datetime, first_trip_datetime, units = "days")), 1)]
-  # 
-  # # Count the number of trips per user
-  # n_trips <- count_total_trips(cons)
-})
+# test_that("participants after summarise_trips_without_trips matches participants after summarise_trips", {
+#   testthat::skip_on_ci()
+# 
+#   trips <- tidy_cleaned_trips(query_cleaned_trips(cons),
+#                               project_crs = get_golem_config("project_crs")
+#   )
+# 
+#   participants_using_trips <-
+#     tidy_participants(query_stage_profiles(cons), query_stage_uuids(cons)) %>%
+#     summarise_trips(., trips)
+# 
+#   participants_using_db_trip_summary <-
+#     tidy_participants(query_stage_profiles(cons), query_stage_uuids(cons)) %>%
+#     summarise_trips_without_trips(., cons)
+# 
+#   waldo::compare(participants_using_db_trip_summary$first_trip_datetime,participants_using_trips$first_trip_datetime)
+#   waldo::compare(participants_using_db_trip_summary$last_trip_datetime,participants_using_trips$last_trip_datetime)
+#   
+#   waldo::compare(participants_using_db_trip_summary$first_trip_local_datetime,participants_using_trips$first_trip_local_datetime)
+#   waldo::compare(participants_using_db_trip_summary$last_trip_local_datetime,participants_using_trips$last_trip_local_datetime)
+#   
+#   
+#   waldo::compare(participants_using_db_trip_summary$n_active_days,participants_using_trips$n_active_days)
+#   waldo::compare(participants_using_db_trip_summary$n_trips_today,participants_using_trips$n_trips_today)
+#   waldo::compare(participants_using_db_trip_summary$n_days,participants_using_trips$n_days)
+#   waldo::compare(participants_using_db_trip_summary$n_trips,participants_using_trips$n_trips)
+#   
+#   
+#   # compare the data frames. 'Participants with each summarise trips method do not match.')
+#   colnames(participants_using_db_trip_summary) == colnames(participants_using_trips)
+#   
+#   # Didn't work the way I expected it to
+#   # for (j in colnames(participants_using_db_trip_summary)) {
+#   #   waldo::compare(participants_using_db_trip_summary[[j]],participants_using_trips[[j]])
+#   # }
+#   
+#   
+#   ### Below are the summary columns to check, as seen in summarise_trips_without_trips
+#   # .[, .(
+#   #   n_trips_today = sum(date == Sys.Date()),
+#   #   n_active_days = data.table::uniqueN(date),
+#   #   first_trip_datetime = min(start_fmt_time),
+#   #   last_trip_datetime = max(start_fmt_time), # in UTC
+#   #   first_trip_local_datetime = format(min(lubridate::as_datetime(start_local_time)), usetz = FALSE),
+#   #   last_trip_local_datetime = format(max(lubridate::as_datetime(end_local_time)), usetz = FALSE)
+#   # ), by = user_id] %>% 
+#   #   .[, n_days := round(as.numeric(difftime(last_trip_datetime, first_trip_datetime, units = "days")), 1)]
+#   # 
+#   # # Count the number of trips per user
+#   # n_trips <- count_total_trips(cons)
+# })
 
 
 test_that("summarise_trips_without_trips returns a nonempty data.table", {
