63279e56995cecec289365a086b07ee78c3e194a
allenmichael099 <allenmichael099@gmail.com>
2021-08-05 16:54:41 -0600

    Add a tolerance to the summarize trips with and without trips check.

--- tests/testthat/test-utils_tidy_data.R
+++ tests/testthat/test-utils_tidy_data.R
@@ -15,8 +15,7 @@ test_that("tidy_cleaned_trips_by_timestamp excludes user input columns when ther
 
 test_that("summarise_trips_without_trips returns a nonempty data.table", {
   testthat::skip_on_ci()
-  tidied_participants <-
-    summary_df <-
+  summary_df <-
     tidy_participants(query_stage_profiles(cons), query_stage_uuids(cons)) %>%
     summarise_trips_without_trips(cons)
 
@@ -40,48 +39,31 @@ test_that("participants after summarise_trips_without_trips matches participants
     tidy_participants(query_stage_profiles(cons), query_stage_uuids(cons)) %>%
     summarise_trips_without_trips(., cons)
   
-  waldo::compare(participants_using_db_trip_summary$first_trip_datetime,participants_using_trips$first_trip_datetime, 
-                 x_arg = "without_trips",y_arg = "with_trips")
-  waldo::compare(participants_using_db_trip_summary$last_trip_datetime,participants_using_trips$last_trip_datetime, 
-                 x_arg = "without_trips",y_arg = "with_trips")
+  trip_summ_fields <- c('first_trip_datetime',
+                        'last_trip_datetime',
+                        'first_trip_local_datetime',
+                        'last_trip_local_datetime',
+                        'n_active_days',
+                        'n_trips_today',
+                        'n_days',
+                        'n_trips'
+  )
   
-  waldo::compare(participants_using_db_trip_summary$first_trip_local_datetime,participants_using_trips$first_trip_local_datetime, 
-                 x_arg = "without_trips",y_arg = "with_trips")
-  waldo::compare(participants_using_db_trip_summary$last_trip_local_datetime,participants_using_trips$last_trip_local_datetime, 
-                 x_arg = "without_trips",y_arg = "with_trips")
-  
-  
-  waldo::compare(participants_using_db_trip_summary$n_active_days,participants_using_trips$n_active_days, 
-                 x_arg = "without_trips",y_arg = "with_trips")
-  waldo::compare(participants_using_db_trip_summary$n_trips_today,participants_using_trips$n_trips_today, 
-                 x_arg = "without_trips",y_arg = "with_trips")
-  waldo::compare(participants_using_db_trip_summary$n_days,participants_using_trips$n_days, 
-                 x_arg = "without_trips",y_arg = "with_trips")
-  waldo::compare(participants_using_db_trip_summary$n_trips,participants_using_trips$n_trips, 
-                 x_arg = "without_trips",y_arg = "with_trips")
-  
-  # compare the data frames. 'Participants with each summarise trips method do not match.')
-  colnames(participants_using_db_trip_summary) == colnames(participants_using_trips)
-  
-  # Didn't work the way I expected it to
-  # for (j in colnames(participants_using_db_trip_summary)) {
-  #   waldo::compare(participants_using_db_trip_summary[[j]],participants_using_trips[[j]])
-  # }
-  
-  
-  ### Below are the summary columns to check, as seen in summarise_trips_without_trips
-  # .[, .(
-  #   n_trips_today = sum(date == Sys.Date()),
-  #   n_active_days = data.table::uniqueN(date),
-  #   first_trip_datetime = min(start_fmt_time),
-  #   last_trip_datetime = max(start_fmt_time), # in UTC
-  #   first_trip_local_datetime = format(min(lubridate::as_datetime(start_local_time)), usetz = FALSE),
-  #   last_trip_local_datetime = format(max(lubridate::as_datetime(end_local_time)), usetz = FALSE)
-  # ), by = user_id] %>% 
-  #   .[, n_days := round(as.numeric(difftime(last_trip_datetime, first_trip_datetime, units = "days")), 1)]
-  # 
-  # # Count the number of trips per user
-  # n_trips <- count_total_trips(cons)
+  for (field in trip_summ_fields){
+    cat(sprintf('Current field: %s...',field))
+    
+    if (field %in% c('first_trip_datetime',
+                     'last_trip_datetime',
+                     'first_trip_local_datetime',
+                     'last_trip_local_datetime')) {
+      expect_equal(participants_using_db_trip_summary[[field]],
+                   participants_using_trips[[field]], tolerance = 1)
+    } else {
+      expect_equal(participants_using_db_trip_summary[[field]],
+                   participants_using_trips[[field]])
+    }
+    cat('Passed\n')
+  }
 })
 
 test_that("summarise_server_calls returns a nonempty data.table", {
