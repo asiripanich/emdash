1ec41342fc31129addbc88070d088e8bb0730e2e
asiripanich <17020181+asiripanich@users.noreply.github.com>
2021-08-06 16:13:23 +1000

    Snap test

--- tests/testthat/test-utils_tidy_data.R
+++ tests/testthat/test-utils_tidy_data.R
@@ -26,44 +26,22 @@ test_that("summarise_trips_without_trips returns a nonempty data.table", {
 
 test_that("participants after summarise_trips_without_trips matches participants after summarise_trips", {
   testthat::skip_on_ci()
-  
+
   trips <- tidy_cleaned_trips(query_cleaned_trips(cons),
-                              project_crs = get_golem_config("project_crs")
+    project_crs = get_golem_config("project_crs")
   )
-  
+
   participants_using_trips <-
     tidy_participants(query_stage_profiles(cons), query_stage_uuids(cons)) %>%
     summarise_trips(., trips)
-  
+
   participants_using_db_trip_summary <-
     tidy_participants(query_stage_profiles(cons), query_stage_uuids(cons)) %>%
     summarise_trips_without_trips(., cons)
-  
-  trip_summ_fields <- c('first_trip_datetime',
-                        'last_trip_datetime',
-                        'first_trip_local_datetime',
-                        'last_trip_local_datetime',
-                        'n_active_days',
-                        'n_trips_today',
-                        'n_days',
-                        'n_trips'
-  )
-  
-  for (field in trip_summ_fields){
-    cat(sprintf('Current field: %s...',field))
-    
-    if (field %in% c('first_trip_datetime',
-                     'last_trip_datetime',
-                     'first_trip_local_datetime',
-                     'last_trip_local_datetime')) {
-      expect_equal(participants_using_db_trip_summary[[field]],
-                   participants_using_trips[[field]], tolerance = 1)
-    } else {
-      expect_equal(participants_using_db_trip_summary[[field]],
-                   participants_using_trips[[field]])
-    }
-    cat('Passed\n')
-  }
+
+  testthat::expect_snapshot(participants_using_trips)
+  testthat::expect_snapshot(participants_using_db_trip_summary)
+
 })
 
 test_that("summarise_server_calls returns a nonempty data.table", {
