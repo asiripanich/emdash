179046eecb789ea8c317718e8d65b2900cbcdc84
Shankari <shankari@eecs.berkeley.edu>
2021-09-14 14:14:26 -0700

    Display the inferred labels correctly
    This fixes https://github.com/asiripanich/emdash/issues/69
    
    It basically changes the rendering for the inferred trips table to JSON stringify the array,
    similar to https://github.com/asiripanich/emdash/commit/eb7921f1
    
    This change works (see screenshots).
    However, it has some hacks that should be cleaned up by somebody with more R knowledge than me.
    
    - Multiple copies of `datatable_options`
    
    The current codebase has two identical copies of `datatable_options`
    https://github.com/asiripanich/emdash/blob/5334c9229febaddaa234a3d1adc62f8f90547a90/R/app_server.R#L167
    and
    https://github.com/asiripanich/emdash/blob/5334c9229febaddaa234a3d1adc62f8f90547a90/R/app_server.R#L195
    
    To be consistent with the existing codebase, I have created a third copy for the trips object.
    In the long-term, we need to unify all of these to ensure DRY
    
    - Hardcoded column for `inferred_trips`
    
    The `inferred_trips` column number is currently hard-coded to 5. This is because, due to removed columns before display,
    
    ```
    inferred_label_index <- which(names(data_geogr$trips) == "inferred_labels")
    ```
    returns 17.
    
    We need to find the index in the list of _displayed_ columns, but I don't
    understand R well enough to understand where and how to make that change.
    
    @asiripanich, could you clean up the hacks, or do you want to merge first?

--- R/app_server.R
+++ R/app_server.R
@@ -278,10 +278,40 @@ app_server <- function(input, output, session) {
   })
 
   observeEvent(data_geogr$click, {
+   # Set the options used by DT::renderDataTable within dtedit and mod_DT
+   datatable_options <- list(
+     scrollX = TRUE,
+     pageLength = 100,
+     dom = "Bfrtip",
+     buttons = c("copy", "csv", "excel", "pdf", "print", "colvis")
+   )
+
+    if ("inferred_labels" %in% colnames(data_geogr$trips)) {
+       message("Found inferred labels column, formatting");
+       inferred_label_index <- which(names(data_geogr$trips) == "inferred_labels")
+       message(inferred_label_index);
+       message(colnames(data_geogr$trips));
+
+       # Add columnDefs to datatable options to convert fmt_inferred_labels to a Date
+       datatable_options[["columnDefs"]] <- list(list(
+         # target column indices start from 0.
+         # However, DT adds a row number column to the display as the 0th column
+         # TODO: Calculate this based on which trip columns are currently hidden
+         # This is currently hard-coded based on the default
+         targets = 5,
+         render = htmlwidgets::JS(
+           "function(data, type, row) {",
+               "return JSON.stringify(data);",
+           "}"
+         )
+       ))
+    }
+
     callModule(mod_DT_server, "DT_ui_trips",
       data = data_geogr$trips %>%
         dplyr::select(-dplyr::any_of(getOption("emdash.cols_to_remove_from_trips_table"))) %>%
-        sf::st_drop_geometry()
+        sf::st_drop_geometry(),
+      DT_options = datatable_options
     )
   })
 
@@ -338,7 +368,6 @@ app_server <- function(input, output, session) {
   observeEvent(filtered_trips()$data_filtered(), {
     # since filtered trips and data filtered are both reactive, need to include parentheses after each. otherwise you get:
     # Error in UseMethod: no applicable method for 'select' applied to an object of class "c('reactiveExpr', 'reactive', 'function')"
-
     callModule(
       mod_mapview_server,
       "mapview_trips",
